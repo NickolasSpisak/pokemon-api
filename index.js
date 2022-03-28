81480af073246a93e1928e39405c13d031bf9acc
spish-web <nickolas.spisak@gmail.com>
2022-03-28 00:37:56 -0400

    first commit

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ app.use("/pokemon", pokemonRoutes);
 app.get("/", (req, res) => {
   res.send("Hello");
 });
-const CONNECTION_URL = `${process.env.CONNECTION_URL}`;
-const PORT = `${process.env.PORT}`;
+const CONNECTION_URL =
+  "mongodb+srv://spish:Lordspish69@cluster0.wdrxo.mongodb.net/pokemon?retryWrites=true&w=majority";
+const PORT = 5002;
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
